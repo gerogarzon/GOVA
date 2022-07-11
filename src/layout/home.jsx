f0716d2136d2562d93b86761d46c4ada40c22f30
gerogarzon <gerogarzon@gmail.com>
2022-07-10 21:33:29 -0300

    Cambie la info de contacto y ajuste los margenes

--- src/layout/home.jsx
+++ src/layout/home.jsx
@@ -8,7 +8,7 @@ const home = () => {
         fluid
         style={{
           textAlign: "center",
-          marginTop: "150px",
+          marginTop: "75px",
         }}
       >
         <Row>
@@ -23,25 +23,28 @@ const home = () => {
         fluid
         style={{
           textAlign: "center",
-          marginTop: "300px",
+          marginTop: "75px",
         }}
       >
         <Row>
-          <h3>Contact us:</h3>
           <Col>
-            <p>
-              Carrer de les Garrigues 8, Piso 01 Puerta 01 (Valencia – España)
-            </p>
+            <h3>Contact us:</h3>
+            <div>Mail: govaarquitectos@gmail.com</div>
+            <div>Instagram: @gova.arquitecos</div>
+            <div>Phone: +54 9 381 5721009 // +54 9 381 4949301</div>
           </Col>
         </Row>
-        <Row>
+
+        <Row
+          style={{
+            marginTop: "10px",
+          }}
+        >
           <Col>
-            <p>govaarquitectos@gmail.com // @gova.arquitecos</p>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <p>+54 9 381 5721009 // +54 9 381 4949301</p>
+            <h3>Visit us:</h3>
+            <div>
+              Carrer de les Garrigues 8, Piso 01 Puerta 01 (Valencia–España)
+            </div>
           </Col>
         </Row>
       </Container>
